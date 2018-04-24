74a4ba129b1ab3bf126a02c8636235c07a511d0c
willtp87 <willtp87@hotmail.com>
2018-04-24 15:29:32 -0300

    metadata display tab

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,14 +1,12 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\islandora_fits\Controller\DefaultController.
- */
-
 namespace Drupal\islandora_fits\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+use AbstractObject;
 
 /**
  * Default controller for the islandora_fits module.
@@ -18,27 +16,17 @@ class DefaultController extends ControllerBase {
   /**
    * Access callback for viewing technical metadata.
    */
-  public function islandora_fits_metadata_access($object, AccountInterface $account) {
-    return islandora_datastream_access('view technical metadata', $object[\Drupal::config('islandora_fits.settings')->get('islandora_fits_techmd_dsid')]);
+  public static function metadataAccess($object, AccountInterface $account) {
+    $object = islandora_object_load($object);
+    $perm = islandora_fits_metadata_access($object);
+    return $perm ? AccessResult::allowed() : AccessResult::forbidden();
   }
 
   /**
    * Technical metadata display.
    */
-  public function islandora_fits_metadata_display($object) {
-    module_load_include('inc', 'islandora', 'includes/breadcrumb');
-    drupal_set_breadcrumb(islandora_get_breadcrumbs($object));
-    // @FIXME
-    // theme() has been renamed to _theme() and should NEVER be called directly.
-    // Calling _theme() directly can alter the expected output and potentially
-    // introduce security issues (see https://www.drupal.org/node/2195739). You
-    // should use renderable arrays instead.
-    //
-    //
-    // @see https://www.drupal.org/node/2195739
-    // $output = theme('islandora_fits_metadata_display', array('islandora_object' => $object));
-
-    return $output;
+  public static function metadataDisplay(AbstractObject $object) {
+    return islandora_fits_metadata_display($object);
   }
 
 }
